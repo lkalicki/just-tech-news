4d52fdda1044a373f4e2e6b127e231921a49bdd7
lkalicki <lkalicki98@gmail.com>
2022-01-15 16:58:18 -0500

    fixing heroku

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 const express = require('express');
 const session = require('express-session');
+const hbs = exphbs.create({ helpers });
 const exphbs = require('express-handlebars');
 
 const app = express();
